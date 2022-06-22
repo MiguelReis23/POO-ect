bbc5bd30327d80fc22a0bfcd45129041ccc548b4
Miguel Reis <r315miguel@gmail.com>
2022-06-22 15:57:05 +0100

    metodo equals

--- src/Estudo/Teste2021/AgenciaTuristica.java
+++ src/Estudo/Teste2021/AgenciaTuristica.java
@@ -70,4 +70,15 @@ public class AgenciaTuristica {
     public String toString(){
         return "Agencia Turistica " + nome + "\n" + "Atividades: " + atividades;
     }
+    
+    @Override
+    public boolean equals(Object obj){
+        if (this==obj) return true;
+        if (obj==null) return false;
+        if (getClass()!=obj.getClass()) return false;
+
+        Atividade atividade=    (Atividade) obj;
+
+        return this.nome.equals(atividade.getNome());
+    }
 }
